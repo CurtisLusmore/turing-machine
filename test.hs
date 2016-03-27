f124c2a1656ed02546bd727f41471539de45f493
Curtis Lusmore <curtis.lusmore@gmail.com>
2016-03-28 09:04:26 +1000

    Changed acceptance to function

--- test.hs
+++ test.hs
@@ -1,15 +1,20 @@
 import Turing
 
-reprStep (q, as, bs) = (reverse $ take 5 as) ++ take 5 bs
+reprStep n (q, as, bs) = (reverse $ take n as) ++ take n bs
+reprStep' = reprStep 5
 
-steps1 = map reprStep $ run ' ' [] [] 1 step []
+startswith []     _      = True
+startswith _      []     = False
+startswith (a:as) (b:bs) = a == b && startswith as bs
+
+steps1 = map reprStep' $ run ' ' [] [] 1 step (const False)
   where
     step (1, _) = (2, '0', MoveRight)
     step (2, _) = (3, ' ', MoveRight)
     step (3, _) = (4, '1', MoveRight)
     step (4, _) = (1, ' ', MoveRight)
 
-steps2 = map reprStep $ run '0' [] ['1', '1'] 1 step [0]
+steps2 = map reprStep' $ run '0' [] ['1', '1'] 1 step (==0)
   where
     step (1, '0') = (0, '0', Stay)
     step (1, '1') = (2, '0', MoveRight)
@@ -23,7 +28,7 @@ steps2 = map reprStep $ run '0' [] ['1', '1'] 1 step [0]
     step (5, '1') = (5, '1', MoveLeft)
     step (q, c)   = error $ show q ++ " " ++ show c
 
-steps3 = map reprStep $ run '0' [] [] 1 step [0]
+steps3 = map reprStep' $ run '0' [] [] 1 step (==0)
   where
     step (1, '0') = (2, '1', MoveRight)
     step (1, '1') = (3, '1', MoveLeft)
--- turing.hs
+++ turing.hs
@@ -3,13 +3,12 @@ module Turing where
 data Move = Stay | MoveLeft | MoveRight
 
 run                             -- run a Turing Machine
-  :: Eq q
-  => c                          -- the blank symbol
+  :: c                          -- the blank symbol
   -> [c]                        -- the input tape to the left
   -> [c]                        -- the input tape to the right including head
   -> q                          -- the starting state
   -> ((q, c) -> (q, c, Move))   -- the transition function
-  -> [q]                        -- the acceptance states
+  -> (q -> Bool)                -- the acceptance function
   -> [(q, [c], [c])]            -- the list of all future states
 run b as bs q0 d f = (q0, as', bs') : run' as' bs' q0 d f
   where
@@ -20,8 +19,8 @@ run b as bs q0 d f = (q0, as', bs') : run' as' bs' q0 d f
         Nothing             -> []   -- halted
         Just (q', as', bs') -> (q', as', bs') : run' as' bs' q' d f
     runOnce (a:as) (b:bs) q0 d f
-        | q0 `elem` f = Nothing     -- halted
-        | otherwise   =
+        | f q0      = Nothing       -- halted
+        | otherwise =
           case d (q0, b) of
             (q', b', Stay)      -> Just (q', a:as, b':bs)
             (q', b', MoveLeft)  -> Just (q', as, a:b':bs)
